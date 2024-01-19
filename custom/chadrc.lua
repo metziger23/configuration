58369868a5d748a05dd4d2074e8c2d6ca5e22d8c
metziger23 <metziger23@gmail.com>
2024-01-19 17:16:23 +0300

    added ReplaceWithRegister plugin that's not lazy loaded now

--- custom/chadrc.lua
+++ custom/chadrc.lua
@@ -2,13 +2,14 @@
 local M = {}
 
 M.ui = {
-  theme_toggle = { "onedark", "one_light" },
-  theme = 'onedark',
+  theme_toggle = { "catppuccin", "one_light" },
+  theme = 'catppuccin',
   nvdash = {
     load_on_startup = true,
   }
 }
 
+M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 
 return M
---
+++ custom/plugins.lua
@@ -0,0 +1,8 @@
+local plugins = {
+  {
+    "inkarkat/vim-ReplaceWithRegister",   -- replace with register contents using motion (gr + motion)
+    lazy = false,
+  },
+}
+
+return plugins
