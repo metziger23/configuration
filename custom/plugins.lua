7e90625855b031b258859381b03c4a5533c43c41
metziger23 <metziger23@gmail.com>
2024-01-19 21:21:58 +0300

    added kylechui/nvim-surround plugin

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -1,8 +1,18 @@
 local plugins = {
   {
-    "inkarkat/vim-ReplaceWithRegister",   -- replace with register contents using motion (gr + motion)
+    "inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
     lazy = false,
   },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  }
 }
 
 return plugins
