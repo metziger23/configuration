56b8ccfee81b0e330e934c15c25dd97aaba6fff5
metziger23 <metziger23@gmail.com>
2024-01-22 19:20:19 +0300

    added clang support

--- custom/mappings.lua
+++ custom/mappings.lua
@@ -23,4 +23,18 @@ M.telescope = {
   }
 }
 
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Add breakpoint at line",
+    },
+    ["<leader>dr"] = {
+      "<cmd> DapContinue <CR>",
+      "Start or continue the debugger",
+    }
+  }
+}
+
 return M
